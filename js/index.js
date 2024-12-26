2ce870d78e79c37b6a2e16d736015e940bcf4739
Josh <aftershock840@gmail.com>
2024-12-26 08:06:52 -0500

    updated javascript file

--- js/index.js
+++ js/index.js
@@ -3,16 +3,13 @@
 "use strict"
 
 
-// let healthPoints = 15;
+// function healthStatus(num) {
+// if (num >= 10) {
+//     console.log("Hero is healthy.");
+// } else if (num < 10 && num >= 5) {
+//     console.log("Hero is slightly damaged.");
+// } else {
+//     console.log("Hero is close to death.");
+// }
 
-
-function healthStatus(num) {
-if (num >= 10) {
-    console.log("Hero is healthy.");
-} else if (num < 10 && num >= 5) {
-    console.log("Hero is slightly damaged.");
-} else {
-    console.log("Hero is close to death.");
-}
-
-}
+// }
