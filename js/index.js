81ffa20cf8ce365fc700d3423073b7b863c92a4d
Josh <aftershock840@gmail.com>
2024-12-26 08:05:50 -0500

    added experimental javascript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+// let healthPoints = 15;
+
+
+function healthStatus(num) {
+if (num >= 10) {
+    console.log("Hero is healthy.");
+} else if (num < 10 && num >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+}
