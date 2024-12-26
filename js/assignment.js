22ee286bf7971ca10efa99be61982554d34d4a31
Josh <aftershock840@gmail.com>
2024-12-26 17:51:16 -0500

    refactored if/else statement to ternary operator

--- js/assignment.js
+++ js/assignment.js
@@ -9,23 +9,30 @@ let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
-
+    let answerOutput = document.getElementById("answer-display");
     // Write your code below
-    if(age >= 21){
-        console.log("You can vote and purchase alcohol.");
-    } else if(age >= 18 && age < 21) {
-        console.log("You can vote, but you cannot purchase alcohol.");
-    } else {
-        console.log("You cannot vote and you cannot purchase alcohol.");
-    };
+    // if(age >= 21){
+    //     console.log("You can vote and purchase alcohol.");
+    // } else if(age >= 18 && age < 21) {
+    //     console.log("You can vote, but you cannot purchase alcohol.");
+    // } else {
+    //     console.log("You cannot vote and you cannot purchase alcohol.");
+    // };
+
+    // ternary operator start
+    let areYouOldEnough = age >=21 ? "You can vote and purchase alcohol"
+    : age >=18 && age < 21 ? "You can vote, but you cannot purchase alcohol"
+    : "You cannot vote and you cannot purchase alcohol.";
+    answerOutput.innerText = areYouOldEnough;
 };
+
 
 
 submissionBtn.addEventListener("click", function () {
     checkAgeAndRespond();
 });
 
-// ask questions about this 
+// ask questions about this
 function validateInput(event) {
     const invalidCharacters = ["e", "E", "-", "0", "+"];
     if (invalidCharacters.includes(event.key)) {
